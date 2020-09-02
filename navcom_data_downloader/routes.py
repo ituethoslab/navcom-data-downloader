ded781a00c75836a0508cc98b8981017df69410f
Mace Ojala <maco@itu.dk>
2020-09-02 18:53:00 +0200

    Added a form for user input.

--- navcom_data_downloader/routes.py
+++ navcom_data_downloader/routes.py
@@ -1,6 +1,17 @@
 from navcom_data_downloader import app
+from flask import render_template, request
 
 @app.route('/hello')
 def hello_world():
     app.logger.debug("Hello world route was requested.")
     return "Hello world."
+
+@app.route('/')
+def index():
+    app.logger.debug("Route %s", "/")
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    app.logger.debug("Route %s", "/submit")
+    return "Imagine data here"
