23d264ab9401254b4751df3d7c4c2c43bc802e71
Mace Ojala <maco@itu.dk>
2020-09-03 16:10:39 +0200

    Query data from Twitter and be a bit resilient about user input.

--- navcom_data_downloader/models.py
+++ navcom_data_downloader/models.py
@@ -1,4 +1,5 @@
 from navcom_data_downloader import app
+from datetime import datetime
 import GetOldTweets3 as got
 
 class DataSource():
@@ -19,13 +20,16 @@ class TwitterDataSource(DataSource):
 
     def query(self, query, max = 10):
         app.logger.debug(f"Querying Twitter for '{query}'")
-        twitter_query = got.manager.TweetCriteria()\
-                           .setQuerySearch(query['string'])\
-                           .setSince(query['start-date'])\
-                           .setUntil(query['end-date'])\
-                           .setMaxTweets(query.get('max', max))
+        twitter_query = got.manager.TweetCriteria()
+        # Twitter won't accept empty query string but will return 400 Bad Request.
+        twitter_query.setQuerySearch(query['string'])
+        if 'start-date' in query:
+            twitter_query.setSince(query['start-date'])
+        if 'end-date' in query:
+            twitter_query.setUntil(query['end-date'])
+        twitter_query.setMaxTweets(max)
+
         tweets = got.manager.TweetManager.getTweets(twitter_query)
         app.logger.debug(tweets)
         
         return tweets
-
--- tests/test_models.py
+++ tests/test_models.py
@@ -8,14 +8,28 @@ class TestDataSource(unittest.TestCase):
 
 
 class TestTwitterDataSource(unittest.TestCase):
-    def test_instantiating_twitter_model_should_be_fine(self):
+    def test_instantiating_should_be_fine(self):
         tds = TwitterDataSource()
         self.assertIsInstance(tds, TwitterDataSource)
     
-    def test_querying_twitter_model_should_params(self):
+    def test_querying_with_all_params_should_return_data(self):
         query = {'string': "giraffe",
                  'start-date': '2020-08-15',
                  'end-date': '2020-09-03'}
         tds = TwitterDataSource()
         data = tds.query(query)
         self.assertEqual(10, len(data))
+
+    def test_querying_with_no_params_should_raise_an_error(self):
+        query = {}
+        tds = TwitterDataSource()
+        with self.assertRaises(KeyError):
+            data = tds.query(query)
+
+    def test_query_with_only_string_should_be_sufficient(self):
+        query = {'string': 'goats'}
+        tds = TwitterDataSource()
+        data = tds.query(query)
+        self.assertEqual(10, len(data))
+
+    
