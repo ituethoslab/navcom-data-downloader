18659197fd470bd7710ae5502aaa0eb463d15854
Mace Ojala <maco@itu.dk>
2020-09-22 23:25:50 +0200

    Successfully retrieves a single submission from Reddit.

--- navcom_data_downloader/models.py
+++ navcom_data_downloader/models.py
@@ -1,5 +1,8 @@
 from navcom_data_downloader import app
 from datetime import datetime
+from config import RedditCredentials
+import pandas as pd
+import praw
 import GetOldTweets3 as got
 import csv
 import io
@@ -57,3 +60,74 @@ class TwitterDataSource(DataSource):
         output_str = output.getvalue()
             
         return output_str
+
+
+class RedditDataSource(DataSource):
+    """Model for Reddit datasource.
+
+    Using the PRAW library."""
+    def __init__(self):
+        super().__init__()
+        app.logger.debug(f"  ... as a Reddit DataSource instantiated")
+        creds = RedditCredentials()
+        self.reddit = praw.Reddit(client_id=creds.client_id,
+                                  client_secret=creds.client_secret,
+                                  password=creds.password,
+                                  user_agent=creds.user_agent,
+                                  username=creds.username)
+
+    def get_submission(self, submission_id):
+        """Get an individual submission and its comments."""
+        submission = self.reddit.submission(id=submission_id)
+        submission.comments.replace_more(limit=None)
+        return submission
+
+    def get_hot(self, subreddit, limit = 10):
+        app.logger.debug(f"Querying Reddit for hot in {subreddit}")
+        submissions = self._query(subreddit, 'hot', limit = limit)
+
+        return submissions
+
+    def get_new(self, subreddit, limit = 10):
+        app.logger.debug("Querying Reddit for new in {subreddit}")
+        submissions = self._query(subreddit, 'new', limit = limit)
+
+        return submissions
+
+    def get_top(self, subreddit, limit = 10):
+        raise NotImplementedError
+
+    def _query(self, subreddit, kind, limit = 10):
+        app.logger.debug(f"Querying Reddit {subreddit} for '{kind}'")
+        subreddit = self.reddit.subreddit(subreddit)
+        if kind == 'hot':
+            submissions = subreddit.hot(limit = limit)
+
+        if kind == 'new':
+            submissions = subreddit.new(limit = limit)
+
+        if kind == 'top':
+            raise NotImplementedError("Needs a view.")
+
+        # Retrieve more comments.
+        # for submission in submissions:
+        #     submission.comments.replace_more()
+
+        return list(submissions)
+
+    def _as_csv(self, submissions):
+        """Format posts as CSV:"""
+        # pd.DataFrame.from_records([vars(c) for s in submissions for c in s.comments])
+        # pd.DataFrame.from_records([vars(c) for s in submissions for c in s.comments])[['author', 'created_utc', 'edited', 'score', 'is_submitter', 'parent_id', 'stickied']]
+        # pd.DataFrame.from_records([{**vars(s), **vars(c)} for s in submissions for c in s.comments])[['title', 'body', 'author', 'created_utc', 'edited', 'score', 'is_submitter', 'parent_id', 'stickied']]
+        # Rather join on submission ID or something? with
+        #    submissions[2].id == submissions[2].comments[0]._submission
+        #    submissions[2].id == submissions[2].comments[0].submission.id
+        columns = ['title', 'body', 'author', 'created_utc', 'edited',
+                   'score', 'is_submitter', 'parent_id', 'stickied']
+
+        joined = [{**vars(s), **vars(c)} for s in submissions for c in s.comments]
+        df = pd.DataFrame.from_records(joined)
+        df['body'] = df['body'].apply(lambda x : x.replace('\n',' '))
+
+        return df[columns].to_csv()
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,11 @@
 import unittest
 import datetime
-from navcom_data_downloader.models import DataSource, TwitterDataSource
+import praw
+import pandas as pd
+from io import StringIO
+from config import RedditCredentials
+from navcom_data_downloader import app
+from navcom_data_downloader.models import DataSource, TwitterDataSource, RedditDataSource
 
 class TestDataSource(unittest.TestCase):
     def test_instantiating_generic_model_should_be_fine(self):
@@ -51,3 +56,57 @@ class TestTwitterDataSource(unittest.TestCase):
         data = tds._query(query)
         [print(str(datum.date), datum.permalink) for datum in data]
         self.assertTrue(all([datum.date <= end_date for datum in data]))
+
+
+class TestRedditDataSource(unittest.TestCase):
+    def setUp(self):
+        self.rds = RedditDataSource()
+        app.logger.setLevel('INFO')
+
+    def test_instantiating_should_be_fine(self):
+        self.assertIsInstance(self.rds, RedditDataSource)
+        self.assertIsInstance(self.rds.reddit, praw.reddit.Reddit)
+
+    def test_authentication_should_succeed(self):
+        ...
+
+    def test_getting_a_specific_submission_by_id_should_succeed(self):
+        sid = '7jgnxm'
+        submission = self.rds.get_submission(sid)
+        self.assertEqual(sid, submission.id)
+
+    def test_getting_hot_from_a_subreddit_should_succeed(self):
+        subreddit = 'dataisbeautiful'
+        submissions = self.rds.get_hot(subreddit, limit=3)
+        self.assertTrue(all([submission.subreddit.display_name == subreddit for submission in submissions]))
+
+    def test_getting_hot_from_a_subreddit_with_limit_should_limit_the_number_of_results(self):
+        subreddit = 'dataisbeautiful'
+        less_submissions = self.rds.get_hot(subreddit, limit=3)
+        self.assertEqual(3, len(less_submissions))
+
+        more_submissions = self.rds.get_hot(subreddit, limit=100)
+        self.assertEqual(100, len(more_submissions))
+
+    def test_serializing_a_single_submission_should_work(self):
+        submission = self.rds.get_submission('7jgnxm')
+        self.assertIsInstance(self.rds._as_csv([submission]), str)
+        self.assertEqual(540, len(self.rds._as_csv([submission])))
+
+    def test_serialized_single_submission_should_parse(self):
+        submission = self.rds.get_submission('7jgnxm')
+        df = pd.read_csv(StringIO(self.rds._as_csv([submission])))
+        self.assertEqual(df.shape, (1, 10))
+
+    def test_serialized_single_submission_should_have_expected_header(self):
+        submission = self.rds.get_submission('7jgnxm')
+        df = pd.read_csv(StringIO(self.rds._as_csv([submission])))
+        self.assertEqual(list(df.columns), ['Unnamed: 0', 'title', 'body', 'author', 'created_utc', 'edited', 'score', 'is_submitter', 'parent_id', 'stickied'])
+
+    def test_serialized_single_submission_should_have_expected_content(self):
+        submission = self.rds.get_submission('7jgnxm')
+        df = pd.read_csv(StringIO(self.rds._as_csv([submission])))
+        self.assertEqual(df.iloc[0]['title'], 'Error with PRAW')
+        self.assertEqual(df.iloc[0]['parent_id'], 't3_7jgnxm')
+        self.assertEqual(df.iloc[0]['created_utc'], 1513140549.0)
+        self.assertEqual(df.iloc[0]['is_submitter'], False)
